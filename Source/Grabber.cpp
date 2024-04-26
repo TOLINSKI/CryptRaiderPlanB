079443f16dac48cba0bcfcbb081d86bd8a823fa5
TOLINSKI <ben.tulchinski@gmail.com>
2024-04-26 16:33:45 +0300

    First commit, add current code files

---
+++ Source/Grabber.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Engine/World.h"
+#include "Grabber.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// FRotator CameraRotation = Super::GetComponentRotation();
+
+	FRotator CameraRotation = GetComponentRotation();
+	FString CameraRotationCoordinates = CameraRotation.ToCompactString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetName());
+	UE_LOG(LogTemp, Display, TEXT("Rotation: %s"), *CameraRotationCoordinates);
+
+	UWorld *World = GetWorld();
+
+	float TimeSeconds = World->GetTimeSeconds();
+
+	UE_LOG(LogTemp, Display, TEXT("Time Seconds: %f"), TimeSeconds);
+
+	// ...
+}
+
